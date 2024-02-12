53daebf167a3e873699be91de9b04e23f7d8b638
Josenei Santana <joseneisatana@yahoo.com>
2024-02-12 11:55:22 -0300

    erro watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 const gulp =require('gulp');
 const  sass =require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function compilaSass(){
     return gulp.src('./source/style/main.scss')
-        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./build/style'));
 }
 
 exports.sass = compilaSass;
+exports.watch =function(){
+    gulp.watch(' source/style/*.scss' , {ignoreItial: false}, gulp.series(compilaSass));
+}
